9c336bc7959396727613178e8a85189cb54d4e80
Guillermo Barbosa <billandassociatesllc@Guillermos-iMac.local>
2021-12-16 20:08:48 -0500

    created crud

--- s_a_p/my_sap/urls.py
+++ s_a_p/my_sap/urls.py
@@ -23,3 +23,18 @@ urlpatterns[
 urlpatterns[
     path("", views.home, name="home"),
 ]
+urlpatterns = [
+    path("", views.EntryListView.as_view(), name="entry-list"),
+    path("entry/<int:pk>", views.EntryDetailView.as_view(), name="entry-detail"),
+    path("create", views.EntryCreateView.as_view(), name="entry-create"),
+    path(
+        "entry/<int:pk>/update",
+        views.EntryUpdateView.as_view(),
+        name="entry-update",
+    ),
+    path(
+        "entry/<int:pk>/delete",
+        views.EntryDeleteView.as_view(),
+        name="entry-delete",
+    ),
+]
--- s_a_p/my_sap/views.py
+++ s_a_p/my_sap/views.py
@@ -1,4 +1,56 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.genric import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
+from djang.contrib import messages
+from django.contrib.messages.views import SuccessMessageMixin
+
+
+class EntryCreateView(CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+
+
+class EntryUpdateView(UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+    default = get_success_url()
+
+    def get_success_url(self):
+        return reverse_lazy("entry-detail", kwargs={"pk": self.entry.id})
+
+
+class EntryDeleteView(DeleteView):
+    model = Entry
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your entry was deleted!"
+
+    def delete(self, request, *args, **kwargs):
+        messages.success(self.request, self.success_message)
+        return super().delete(request, *args, **kwargs)
+
+
+class EntryCreateView(SuccessMessageMixin, CreateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_url = reverse_lazy("entry-list")
+    success_message = "Your new entry was created!"
+
+
+class EntryUpdateView(SuccessMessageMixin, UpdateView):
+    model = Entry
+    fields = ["title", "content"]
+    success_message = "Your entry was updated!"
+
+    def get_success_url(self):
+        return reverse_lazy("entry-detail", kwargs={"pk": self.object.pk})
+
 
 # Create your views here.
 def blog(request):
