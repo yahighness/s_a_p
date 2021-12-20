c4e5164c51a5689cdcca94256fde909d7675f4ba
mbrown2330 <Mbrown2330@gmail.com>
2021-12-19 22:01:40 -0500

    new work

--- s_a_p/my_sap/apps.py
+++ s_a_p/my_sap/apps.py
@@ -1,6 +1,4 @@
 from django.apps import AppConfig
-
-
 class MySapConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'my_sap'
--- s_a_p/my_sap/views.py
+++ s_a_p/my_sap/views.py
@@ -1,31 +1,23 @@
 from django.shortcuts import render
-
 # Create your views here.
 def blog(request):
-    return render(request)
-    if request.method == "POST":
-        return render(
-            request=request,
-            template_name="blog.html",
-            context={"source": source},
-        )
-
-
-def resource(request):
-    return render(request)
+    # return render(request)
     if request.method == "GET":
-        print(Resource.objects.all())
-        # print(data.resource_id)
         return render(
-            request=request,
-            template_name="resource.html",
-            context={"source": source},
+            request, "blog.html",
         )
-
-
+def resource(request):
+    # return render(request)
+    if request.method == "GET":
+    #     print(Resource.objects.all())
+    #     # print(data.resource_id)
+        return render(request,'resource.html')
+            # request=request,
+            # template_name="resource.html",
+            # context={"source": source},
+        # )
 def home(request):
-    return render(request)
-
-
+    return render(request, 'home.html')
 def client(request):
-    return render(request)
+    return render(request, 'client.html')
+ 
