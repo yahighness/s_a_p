bb18132be1eef2a7c7991354e975798f94d2b0fd
timamonteiro <timamonteiro@icloud.com>
2021-12-19 18:22:24 -0600

    urls.py

--- s_a_p/my_sap/urls.py
+++ s_a_p/my_sap/urls.py
@@ -1,9 +1,41 @@
-from django.urls import path
+from django.contrib import admin
+from django.urls import path, include, resource, home, client
+
+
+
 from . import views
 
-urlpatterns=[
-    path("home/", views.home, name="home"),
-    path("blog/",views.blog,name="blog"),
-    path("resource/", views.resource, name="resource"),
-    path("client/", views.client, name="client")
+
+
+urlpatterns = [
+    path(“admin/“, admin.site.urls), 
+    path(“”, include(“my_sap.urls”)),
+]
+urlpatterns[
+    path(
+        “”,
+        views.profile,
+        name=“profile”,
+    ),
+]
+urlpatterns[
+    path(“”, views.resource, name=“resource”),
+]
+urlpatterns[
+    path(“”, views.home, name=“home”),
+]
+urlpatterns = [
+    path(“”, views.EntryListView.as_view(), name=“entry-list”),
+    path(“entry/<int:pk>“, views.EntryDetailView.as_view(), name=“entry-detail”),
+    path(“create”, views.EntryCreateView.as_view(), name=“entry-create”),
+    path(
+        “entry/<int:pk>/update”,
+        views.EntryUpdateView.as_view(),
+        name=“entry-update”,
+    ),
+    path(
+        “entry/<int:pk>/delete”,
+        views.EntryDeleteView.as_view(),
+        name=“entry-delete”,
+    ),
 ]
