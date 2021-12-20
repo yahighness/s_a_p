bed6dbd871f895028dcd18314b8d0a310667a836
timamonteiro <timamonteiro@icloud.com>
2021-12-20 06:55:18 -0600

    my_sap/urls.py

--- s_a_p/my_sap/urls.py
+++ s_a_p/my_sap/urls.py
@@ -1,41 +1,37 @@
-from django.contrib import admin
-from django.urls import path, include, resource, home, client
+from django.shortcuts import render
 
+# Create your views here.
+def blog(request):
+    # return render(request)
+    if request.method == "GET":
+        return render(
+            request, "blog.html",
+        )
+def resource(request):
+    # return render(request)
+    if request.method == "GET":
+    #     print(Resource.objects.all())
+    #     # print(data.resource_id)
+        return render(request,'resource.html')
+            # request=request,
+            # template_name="resource.html",
+            # context={"source": source},
+        # )
+def home(request):
+    return render(request, 'home.html')
+def client(request):
+    return render(request, 'client.html')
 
-
+from django.urls import path
 from . import views
+urlpatterns=[
+    path("home/", views.home, name="home"),
+    path("blog/",views.blog,name="blog"),
+    path("resource/", views.resource, name="resource"),
+    path("client/", views.client, name="client")
+]
 
 
 
-urlpatterns = [
-    path(“admin/“, admin.site.urls), 
-    path(“”, include(“my_sap.urls”)),
-]
-urlpatterns[
-    path(
-        “”,
-        views.profile,
-        name=“profile”,
-    ),
-]
-urlpatterns[
-    path(“”, views.resource, name=“resource”),
-]
-urlpatterns[
-    path(“”, views.home, name=“home”),
-]
-urlpatterns = [
-    path(“”, views.EntryListView.as_view(), name=“entry-list”),
-    path(“entry/<int:pk>“, views.EntryDetailView.as_view(), name=“entry-detail”),
-    path(“create”, views.EntryCreateView.as_view(), name=“entry-create”),
-    path(
-        “entry/<int:pk>/update”,
-        views.EntryUpdateView.as_view(),
-        name=“entry-update”,
-    ),
-    path(
-        “entry/<int:pk>/delete”,
-        views.EntryDeleteView.as_view(),
-        name=“entry-delete”,
-    ),
-]
+
+
