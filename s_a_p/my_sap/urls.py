40cd65c569c06872e6d75b64b48ec1790eab1a2e
yahighness <90288035+yahighness@users.noreply.github.com>
2022-01-23 19:53:12 -0500

    redirect empty path to home page

--- s_a_p/my_sap/urls.py
+++ s_a_p/my_sap/urls.py
@@ -1,30 +1,8 @@
-from django.shortcuts import render
-
-# Create your views here.
-def blog(request):
-    # return render(request)
-    if request.method == "GET":
-        return render(
-            request, "blog.html",
-        )
-def resource(request):
-    # return render(request)
-    if request.method == "GET":
-    #     print(Resource.objects.all())
-    #     # print(data.resource_id)
-        return render(request,'resource.html')
-            # request=request,
-            # template_name="resource.html",
-            # context={"source": source},
-        # )
-def home(request):
-    return render(request, 'home.html')
-def client(request):
-    return render(request, 'client.html')
-
 from django.urls import path
+from django.views.generic.base import RedirectView
 from . import views
 urlpatterns=[
+    path("", RedirectView.as_view(url="/home/")),
     path("home/", views.home, name="home"),
     path("blog/",views.blog,name="blog"),
     path("resource/", views.resource, name="resource"),
