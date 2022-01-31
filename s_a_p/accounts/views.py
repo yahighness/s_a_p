bca73d657909c2b513c934e6dafcc02db6a1931b
timamonteiro <timamonteiro@icloud.com>
2022-01-30 19:22:59 -0600

    Finish account creation and login logic"

--- s_a_p/accounts/views.py
+++ s_a_p/accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import redirect, render
 from django.contrib.auth import authenticate, login as auth_login
+from django.contrib.auth.models import User
 
 def login(request):
     response = render(request, "login.html")
@@ -12,6 +13,13 @@ def login(request):
 
 
 def create_account(request):
-    response = render(request, "create_account.html")
+    response = render(request, "create_account.html") 
+    if request.method == "POST":
+        username = request.POST["username"]
+        user, created = User.objects.get_or_create(username=username, defaults={"first_name": request.POST["first_name"], "last_name": request.POST["last_name"], "email": request.POST['email']})
+        if created:
+            user.set_password(request.POST['password'])
+            user.save()
+        response = redirect('login')
     return response
 
